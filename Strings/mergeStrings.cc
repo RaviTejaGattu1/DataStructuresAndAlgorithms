3f61caf3faf1087276f062c26ccce645776af749
Ravi Teja Gattu <46156268+RaviTejaGattu1@users.noreply.github.com>
2024-03-24 00:43:51 -0700

    Update and rename strings.cc to mergeStrings.cc
    Code Correction

--- Strings/mergeStrings.cc
+++ Strings/mergeStrings.cc
@@ -7,8 +7,10 @@ using std::string;
 using std::vector;
 using std::prev;
 using std::endl;
+using std::cerr;
  
 string mergeStrings(const string &a, const string &b){
+  string c;
   if(a.empty()){
     return b;
   }
@@ -16,40 +18,36 @@ string mergeStrings(const string &a, const string &b){
     return a;
   }
   else if(a.empty() && b.empty()){
-    perror<<"both strings empty";
+    cerr<<"both strings empty";
   }
   else {
     string::const_iterator ait=a.begin(),bit=b.begin(),cit=c.begin();    
     while(ait!=a.end() && bit!=b.end()){
-      c.pushback(*ait);
+      c.push_back(*ait);
       ait++;
-      c.pushback(*bit);
+      c.push_back(*bit);
       bit++;
     }
-    if(ait==prev(str.end())){
-      while(bit!=b.end()){
-	c.pushback(*bit);
-	bit++;
-      }
+    while(ait!=a.end()){
+      c.push_back(*ait);
+      ait++;
     }
-    if(bit==prev(str.end())){
-      while(bit!=b.end()){
-	c.pushback(*bit);
-	bit++;
-      }
+    while(bit!=b.end()){
+      c.push_back(*bit);
+      bit++;
     }
-    return c;
   }
+  return c;
 }
 
 
 int main(){
-  string a="abc";
-  string b="def";
+  string a="123";
+  string b="efgh";
 
   string c=mergeStrings(a,b);
   cout<<"after merging : "<<c<<endl;
   
-
   return 0;
 }
+
